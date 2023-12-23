55f446f6149755247cbaf091982d4a7fdafd2e24
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-12-23 10:42:03 +0530

    Update compiler and flags for cross-compilation

--- config.sh
+++ config.sh
@@ -4,9 +4,9 @@ PROJECTS="libc kernel"
 export MAKE=${MAKE:-make}
 export HOST=${HOST:-$(./default-host.sh)}
 
-export AR=${HOST}-ar
-export AS=${HOST}-as
-export CC=${HOST}-gcc
+export AR=llvm-ar
+export AS=llvm-as
+export CC=clang
 
 export PREFIX=/usr
 export EXEC_PREFIX=$PREFIX
@@ -14,7 +14,7 @@ export BOOTDIR=/boot
 export LIBDIR=$EXEC_PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
-export CFLAGS='-O2 -g'
+export CFLAGS='-O2 -g -ffreestanding -target i386-pc-none-elf -march=i386 -m32 -Wall -Wextra'
 export CPPFLAGS=''
 
 # Configure the cross-compiler to use the desired system root.
